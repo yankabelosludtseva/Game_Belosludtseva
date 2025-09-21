da854594e8d550506fc1c785a385e3991d0b0b9f
yankabelosludtseva <nblsldcv@gmail.com>
2025-09-21 13:45:01 +0500

    Выполненная практическая работа. Белослудцева

--- пр 3/MainWindow.xaml.cs
+++ пр 3/MainWindow.xaml.cs
@@ -1,9 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using System.Windows.Threading;
-
 
 namespace пр_3
 {
@@ -11,34 +20,24 @@ namespace пр_3
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
         public Classes.PersonInfo Player = new Classes.PersonInfo("Student", 100, 10, 1, 0, 0, 5);
         public List<Classes.PersonInfo> Enemys = new List<Classes.PersonInfo>();
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public Classes.PersonInfo Enemy;
-
         public MainWindow()
         {
             InitializeComponent();
             UserInfoPlayer();
-
             Enemys.Add(new Classes.PersonInfo("Враг 1", 100, 20, 1, 15, 5, 20));
             Enemys.Add(new Classes.PersonInfo("Враг 2", 20, 5, 1, 5, 2, 5));
             Enemys.Add(new Classes.PersonInfo("Враг 3", 50, 3, 1, 10, 10, 15));
-
             dispatcherTimer.Tick += AttackPlayer;
             dispatcherTimer.Interval = new System.TimeSpan(0, 0, 10);
             dispatcherTimer.Start();
-
             SelectEnemy();
-
         }
-        private void AttackPlayer(object sender, System.EventArgs e)
-        {
-            Player.Healht -= Convert.ToInt32(Enemy.Damage * 100f / (100f - Player.Armor));
-            UserInfoPlayer();
-        }
-
         public void UserInfoPlayer()
         {
             if (Player.Glasses > 100 * Player.Level)
@@ -55,9 +54,27 @@ namespace пр_3
             playerGlasses.Content = "Опыт: " + Player.Glasses;
             playerMoney.Content = "Монеты: " + Player.Money;
         }
+
         private void AAttackEnemy(object sender, MouseButtonEventArgs e)
         {
-
+            Enemy.Healht -= Convert.ToInt32(Player.Damage * 100f / (100f - Enemy.Armor));
+            if (Enemy.Healht < 0)
+            {
+                Player.Glasses += Enemy.Glasses;
+                Player.Money += Enemy.Money;
+                UserInfoPlayer();
+                SelectEnemy();
+            }
+            else
+            {
+                emptyHealht.Content = "Жизненные показатели: " + Enemy.Healht;
+                emptyArmor.Content = "Броня: " + Enemy.Armor;
+            }
+        }
+        private void AttackPlayer(object sender, System.EventArgs e)
+        {
+            Player.Healht -= Convert.ToInt32(Enemy.Damage * 100f / (100f - Player.Armor));
+            UserInfoPlayer();
         }
         public void SelectEnemy()
         {
@@ -71,6 +88,5 @@ namespace пр_3
                 Enemys[Id].Money,
                 Enemys[Id].Damage);
         }
-
     }
 }
