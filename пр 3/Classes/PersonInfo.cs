e8c6d3a8d293c4b6086dfc6a4730632011992727
yankabelosludtseva <nblsldcv@gmail.com>
2025-09-21 13:10:26 +0500

    Разработанный класс. Белослудцева

---
+++ пр 3/Classes/PersonInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр_3.Classes
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public int Healht { get; set; }
+        public int Armor { get; set; }
+        public int Level { get; set; }
+        public int Glasses { get; set; }
+        public int Money { get; set; }
+        public float Damage { get; set; }
+
+    }
+}
